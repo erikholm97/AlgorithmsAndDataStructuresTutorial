ae2adac50095a9fdc90efa8c6ca5d5612dcc0c9f
Erik Holm <erikholm97@hotmail.com>
2023-05-31 07:16:09 +0200

    Added linked list class references

---
+++ linked_list.py
@@ -0,0 +1,62 @@
+class Node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list.
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+    
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+
+n1 = Node(10)
+
+print(n1)
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+    head = None
+    next_node = None
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+    
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes 0(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+    
+    def is_empty(self):
+        return self.head == None
+        
+    def size(self):        
+        count += 1
+        current = current.next_node
+
+        
+l = LinkedList()
+
+N1 = Node(10)
+l.head = N1
+
+l.head = N1
+
+l.size()
