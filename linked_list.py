a1f64786d26df7b99cc33565ac780d49b7aaa67c
Erik Holm <erikholm97@hotmail.com>
2023-06-05 07:19:43 +0200

    Added linked list search function

--- linked_list.py
+++ linked_list.py
@@ -6,8 +6,9 @@ class Node:
     data = None
     next_node = None
 
-    def __init__(self, data):
+    def __init__(self, data, next_node = None):
         self.data = data
+        self.next_node = next_node
     
     def __repr__(self):
         return "<Node data: %s>" % self.data
@@ -55,6 +56,40 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Return the node or 'None' if not found
+
+        Takes 0(n) time.
+        """    
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+        else:
+            current = current.next_node
+        return None
+
+    def __repr__(self):
+        """
+        Returns a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return  '-> '.join(nodes)
         
 
         
