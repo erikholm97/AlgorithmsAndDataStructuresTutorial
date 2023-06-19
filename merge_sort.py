729c77baaeeff1143996523db6ac49ed7ae561ac
Erik Holm <erikholm97@hotmail.com>
2023-06-19 08:28:50 +0200

    Added sorting validating functions

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,4 @@
-def merge_sort(list):
+def merge_sort_list(list):
     """
     Sorts a list in ascending order
     Returns a new sorted list
@@ -6,14 +6,16 @@ def merge_sort(list):
     Divide: Find the midpoint of the list and divide into sublists
     Conquer: Recursively sort the sublists created in previous step
     Combine: Mege the sorted sublists created in previous step
+
+    Takes 0(n log n) time
     """
 
     if len(list) <= 1:
         return list
     
     left_half,right_half = split(list)
-    left = merge_sort(left_half)
-    right = merge_sort(right_half)
+    left = merge_sort_list(left_half)
+    right = merge_sort_list(right_half)
 
     return merge_sort(left, right)
 
@@ -21,6 +23,8 @@ def split(list):
     """
     Divide the unsorted list at midpoint into sublists.
     Returns two sublists - left and right
+
+    Takes overall n(log n) time
     """
 
     mid = len(list)//2
@@ -33,6 +37,8 @@ def merge_sort(left, right):
     """
     Merges two lists (arrays), sorting them in the process
     Returns a new merged list
+
+    Runs in overall 0(n) time
     """
 
     l = []
@@ -56,3 +62,19 @@ def merge_sort(left, right):
     while j < len(right):
         l.append(right[j])
         j+=1
+
+    return l
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n== 0 or n == 1:
+        return True
+    
+    return list[0] < list[1] and verify_sorted(list[1:])
+
+alist = [54,62,93,17,77,31,44,55,20]
+l = merge_sort_list(alist)
+
+print(verify_sorted(alist))
+print(verify_sorted(l))
